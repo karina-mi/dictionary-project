008dfec5d3080029427bc912b2d0158a3cf72f22
karinamiheenko <karina.mi.08.08@gmail.com>
2024-06-29 11:02:18 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from './Dictionary'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Hello</h1>
-      </header>
+      <div className="container">
+        <header>
+          <h1>Hello</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+         <small>Coded by Karyna Mikhieienko</small>
+        </footer>
+      </div>
     </div>
   );
 }
