e98965c07ef943187f70e2736c7bf5c87dc8cf07
karinamiheenko <karina.mi.08.08@gmail.com>
2024-06-29 20:36:36 -0700

    created Dictionary

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header>
-          <h1>Hello</h1>
+        <header className="logo">
+          <h1>Dictionary 📚</h1>
         </header>
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer">
-         <small>Coded by Karyna Mikhieienko</small>
-        </footer>
+        <footer>This project was coded by Karyna Mikhieienko and is <a href="https://github.com/karina-mi/dictionary-project" target="_blank" rel="noopener noreferrer">
+          open-sourced on GitHub</a> and <a href="https://famous-crisp-87d456.netlify.app/" target="_blank" rel="noopener noreferrer">
+          hosted on Netlify</a></footer>
       </div>
     </div>
   );
